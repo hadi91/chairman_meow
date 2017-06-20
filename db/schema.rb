7cf4e1ae5788ffd16209a285b0da47d11664b271
Hadi <ahmad_nurhadi@hotmail.com>
2017-06-20 13:29:07 +0800

    Update Schema

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170619151224) do
     t.integer "quantity", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
