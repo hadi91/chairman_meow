606a33ec669bbb8ff926dd76ce1c6391173792e3
ashrafmansur <ashraf261090@gmail.com>
2017-06-20 17:56:43 +0800

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,19 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  # resources :products, only: [:index, :show]
-  # resource :cart, except: [:new, :create]
-#
-  # namespace :shopping do
-  # resources :orders, except: [:edit, :update]
-  # resources :products, only: [:index, :show]
-  # resource :cart, except: [:new, :create]
-  # end
-#
-  # namespace :admin do
-  # resources :orders, except: [:new, :create, :destroy]
-  resources :products
-  # end
-#
+  #resources :products, only: [:index, :show]
+  #resource :shopping_cart, except: [:new, :create]
+
+  namespace :meow do
+    resources :orders, except: [:edit, :update]
+    resources :products, only: [:index, :show]
+    resource :shopping_cart, except: [:new, :create]
+  end
+
+  namespace :admin do
+    resources :orders, except: [:new, :create, :destroy]
+    resources :products
+  end
+
   root "products#index"
 end
