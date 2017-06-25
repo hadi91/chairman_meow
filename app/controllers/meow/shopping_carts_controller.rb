56b471527a43ed44c651b9d2788b0ebf3ffae516
ssoonsl <ssoonsl616+gh@gmail.com>
2017-06-26 00:24:42 +0800

    Edit shopping cart to allow update to 0 quantity that prompts deletion of line item

--- app/controllers/meow/shopping_carts_controller.rb
+++ app/controllers/meow/shopping_carts_controller.rb
@@ -3,6 +3,11 @@ class Meow::ShoppingCartsController < ApplicationController
   def show
     @shopping_cart = current_cart
     @line_items = @shopping_cart.line_items
+    @line_items.each do |line_item|
+      if line_item.quantity == 0
+        line_item.destroy
+      end
+    end
   end
 
 end
